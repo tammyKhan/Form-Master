859fcd58f6ec3019ea527831cc026cd31b0d3863
tammykhan <tammykhan543@gmail.com>
2024-11-05 13:05:40 +0600

    Explore Ref and controlled and uncontrolled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
+import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
-import StatefulForm from './components/statefulForm/statefulForm'
+// import StatefulForm from './components/statefulForm/statefulForm'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
